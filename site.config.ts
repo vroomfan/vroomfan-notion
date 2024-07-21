9e1228a281e039aec7d5d3069c813cc2c5f7c117
vroomfan <vroomfan.dev@gmail.com>
2024-07-21 18:18:58 +0900

    add resume link to header

--- site.config.ts
+++ site.config.ts
@@ -53,16 +53,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Resume',
+      url: 'https://www.rallit.com/resumes/5612@jyp930/%EB%B0%95%EC%9A%A9%EC%A3%BC?theme=STANDARD'
+    },
+    // {
+    //   title: 'Contact',
+    //   pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+    // }
+  ]
 })
